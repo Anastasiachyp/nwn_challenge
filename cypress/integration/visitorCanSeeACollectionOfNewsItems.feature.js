8b0fd95a2b64b14deafaeb7ee4da4d0157b46703
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-20 14:49:40 +0100

    add spy cypress test and new block og it

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,10 +1,21 @@
 describe("A User can,", () => {
   beforeEach(() => {
+    cy.intercept("GET", "https://newsapi.org/v2/top-headlines**").as(
+      "fetchData"
+    );
     cy.visit("/");
   });
+
   it("see the news list correctly", () => {
-    cy.get("[data-cy='news-index']").within(() => {
-      cy.contains("DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017");
+    cy.get("[data-cy='news-1']").within(() => {
+      cy.contains(
+        "DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017"
+      );
+    });
+    it("is expected to return an array of data", () => {
+      cy.get("@fetchData")
+        .its("response.data.articles")
+        .should("be.an", "array");
     });
   });
 });
--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import NewsCard from './NewsCard'
 
 
