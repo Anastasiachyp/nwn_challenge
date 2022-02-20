e64ae6d8c5699bd376389f92d98a42ea68dd1ba2
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-20 14:00:02 +0100

    add test for user can see news and modify Card component

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,5 +1,10 @@
-describe('', () => {
-  it('', () => {
-    cy.visit('/')
+describe("A User can,", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("see the news list correctly", () => {
+    cy.get("[data-cy='news-index']").within(() => {
+      cy.contains("DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017");
+    });
   });
 });
