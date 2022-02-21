bab663297c2abaa005e84d852a9099619b66885a
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-21 01:11:21 +0100

    add useDispatch and useSelector to NewsIndex.jsx

--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import NewsCard from './NewsCard'
 
 
 const NewsIndex = () => {
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.newsFeed);
   return (
     <>
       <NewsCard />
